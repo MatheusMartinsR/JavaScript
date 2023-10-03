a3de52d73a46029e058b4e0b10cd53eec63cbfc6
MatheusMartinsR <martinsmatheusrh08@gmail.com>
2023-10-03 16:57:27 -0300

    avançando_exs

---
+++ avançando_ex.js
@@ -0,0 +1,32 @@
+//Ex 1
+//const lista = []
+//lista.push("Arroz", "Feijão", "Acuçar", "Sal", "Tomate")
+//console.log(lista)
+//lista.shift()
+//console.log(lista)
+
+
+//Ex 2
+//const numeros = [3,7,14,21,29,36,42]
+//const numerosDivisiveis = numeros.find(num => num % 7 == 0);
+//console.log(numerosDivisiveis)
+//const numerosMaior = numeros.find(num => num > 10);
+//console.log(numerosMaior)
+
+
+//Ex 4
+//const numeros = [5,10,15,20,25,30,35,40]
+//const numerosMaiores = numeros.filter((num) => num > 20);
+//console.log(numerosMaiores)
+
+
+//Ex 4
+//const string = " Bom dia, mundo! "
+//const palavra = string.trim().split(" ")
+//console.log(palavra)
+
+
+//5
+//const frase = "O rato roeu a roupa do rei de Roma"
+//console.log(frase.startsWith("O"))
+//console.log(frase.endsWith("Roma"))
